9f383dec73fc2b7dea6ed06645cef4bfb62cdb5b
DanielBonnery <daniel.bonnery@gmail.com>
2016-04-11 10:52:47 -0400

    a

--- demo/table2.R
+++ demo/table2.R
@@ -6,6 +6,7 @@ sampleparam<-list(proph=c(.7,.3),tauh=c(1/70,2/15));
 Param<-list(list(sigma=0.1),list(sigma=1),list(sigma=10));
 N<-5000;nbreps<-400;
 theta=1.5;xi=2
+
 nomparam="$\\sigma$"
 method="grille"
 simulation_data<-lapply(Param,function(param){Simulation_data(nbreps,popmodelfunction,sampleparam,N,theta,xi,param,method)})
