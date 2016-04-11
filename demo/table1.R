be1054b3ecd66e9b399c2af73cef854e659691b0
DanielBonnery <daniel.bonnery@gmail.com>
2016-04-11 11:02:35 -0400

    a

--- demo/table1.R
+++ demo/table1.R
@@ -4,7 +4,6 @@ popmodelfunction<-model.Pareto.bernstrat;
 sampleparam<-list(tauh=c(0.01,0.1));N<-10000;theta<-4;Xi=list(.1,1,2);
 param<-list();nbreps<-400;method="grille";
 nomparam<-c("$\\theta$")
-
 simulation_data<-lapply(Xi,function(xi){Simulation_data(nbreps,popmodelfunction,sampleparam,N,theta,xi,param,method)})
 x=generetableau(simulation_data,nomparam)
 try(system(paste0("evince ",x,grep(".pdf",list.files(path=x),value=TRUE))))
