d4fb14c211528d3330da4f11d39aa32c2bd0b40a
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-10 10:51:26 -0400

    a

--- R/Sample_designs.R
+++ R/Sample_designs.R
@@ -6,50 +6,50 @@
 # - a function demarc, that may have different meanings
 
 #1. Simple random sampling
-#param :liste param$tau : taux de sondage
-SRS<-function(param){
+#sampleparam :liste sampleparam$tau : taux de sondage
+SRS<-function(sampleparam){
   list(
-    param=param,
-    S=function(z){sample(1:length(z),size=floor(param$tau*length(z)),replace=FALSE)},
-    Pik=function(z){rep(floor(param$tau*length(z))/length(z),length(z))},
+    sampleparam=sampleparam,
+    S=function(z){sample(1:length(z),size=floor(sampleparam$tau*length(z)),replace=FALSE)},
+    Pik=function(z){rep(floor(sampleparam$tau*length(z))/length(z),length(z))},
     demarc=function(z){NULL})}  
 
 #2. Stratified sampling
-#param :liste param$tauh : vecteur des taux de sondage par strate
-#param$proph : vecteur des taille en proportion de la pop de chaque strate.
-StratS<-function(param){
+#sampleparam :liste sampleparam$tauh : vecteur des taux de sondage par strate
+#sampleparam$proph : vecteur des taille en proportion de la pop de chaque strate.
+StratS<-function(sampleparam){
   list(
-    param=param,
+    sampleparam=sampleparam,
     Pik=function(z){
       N=length(z)
-      Nh<-floor(param$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
-      nh<-floor(param$tauh*Nh);
+      Nh<-floor(sampleparam$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
+      nh<-floor(sampleparam$tauh*Nh);
       Pikk<-rep(nh/Nh,Nh)[rank(z)]
       return(Pikk)},
     S=function(z){
       N=length(z)
-      Nh<-floor(param$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
-      nh<-floor(param$tauh*Nh);
+      Nh<-floor(sampleparam$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
+      nh<-floor(sampleparam$tauh*Nh);
       unlist(sapply(1:length(Nh),function(i){
         sample((1:N)[(rank(z)>c(0,cumsum(Nh))[i])&(rank(z)<=cumsum(Nh)[i])],nh[i])}))
       },
     demarc=function(z){
       N=length(z)
-      Nh<-floor(param$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
+      Nh<-floor(sampleparam$proph*N);Nh[length(Nh)]=Nh[length(Nh)]+N-sum(Nh)
       z[order(z)][cumsum(Nh)[-length(Nh)]]
       })}
 
 #3. Stratified sampling 2 : z is the name of strata
-#param :liste param$tauh : vecteur des taux de sondage par strate
-#param$tauh : vecteur des taille en proportion de la pop de chaque strate.
-StratS2<-function(param){
+#sampleparam :liste sampleparam$tauh : vecteur des taux de sondage par strate
+#sampleparam$tauh : vecteur des taille en proportion de la pop de chaque strate.
+StratS2<-function(sampleparam){
   list(
-    param=param,
+    sampleparam=sampleparam,
     Pik=function(z){
       ooo=sort(unique(z));
       Pikk<-rep(NULL,length(z));
       for (i in 1:(length(ooo))){
-        Pikk[z==ooo[i]]<-param$tauh[i]}
+        Pikk[z==ooo[i]]<-sampleparam$tauh[i]}
       return(Pikk)},
     S=function(z){
       oo<-rank(z);
@@ -57,7 +57,7 @@ StratS2<-function(param){
       Nh<-as.vector(table(oo))
       nh<-vector()
       Sam<-vector();cum<-0
-      for (i in 1:(length(Nh))){nh[i]<-floor(param$tauh[i]*Nh[i]);
+      for (i in 1:(length(Nh))){nh[i]<-floor(sampleparam$tauh[i]*Nh[i]);
       Sam<-c(Sam,sample((1:N)[(oo>cum)&(oo<=cum+Nh[i])],size=nh[i],replace=FALSE));cum<-cum+Nh[i]}
       return(Sam)},
     demarc=function(z){
@@ -70,82 +70,82 @@ StratS2<-function(param){
       for (i in 1:(length(Nh)-1)){cum<-cum+Nh[i];dem[i]<-sort(z)[cum]}
       return(dem)})}
 #4. Stratifi? avec Bernoulli dans chaque strate
-StratBern<-function(param){
+StratBern<-function(sampleparam){
   list(
-    param=param,
+    sampleparam=sampleparam,
     Pik=function(z){
       ooo=sort(unique(z));
       Pikk<-rep(NULL,length(z));
       for (i in 1:(length(ooo))){
-        Pikk[z==ooo[i]]<-param$tauh[i]}
+        Pikk[z==ooo[i]]<-sampleparam$tauh[i]}
       return(Pikk)},
     S=function(z){
       ooo=sort(unique(z));N<-length(z);
       Pikk<-rep(NULL,length(z));
       for (i in 1:(length(ooo))){
-        Pikk[z==ooo[i]]<-param$tauh[i]}
+        Pikk[z==ooo[i]]<-sampleparam$tauh[i]}
       return((1:N)[runif(N)<Pikk])},
     Sind=function(z){
       ooo=sort(unique(z));N<-length(z);
       Pikk<-rep(NULL,length(z));
       for (i in 1:(length(ooo))){
-        Pikk[z==ooo[i]]<-param$tauh[i]}
+        Pikk[z==ooo[i]]<-sampleparam$tauh[i]}
       return(runif(N)<Pikk)},
     demarc=function(z){
       return(sort(unique(z)))})}
 #5. Sampling with replacement and probability proportional to size
-SWRPPS<-function(param){
-  paramtau<-param$tau
+SWRPPS<-function(sampleparam){
+  sampleparamtau<-sampleparam$tau
   return(
-  list(param=param,
-    Pik=function(z){1-(1-z/sum(z))^(floor(paramtau*length(z)))},#Pik=function(z){z/sum(z)},
-    S=function(z){sample(1:length(z),size=floor(paramtau*length(z)),prob=z/sum(z),replace=TRUE)},
+  list(sampleparam=sampleparam,
+    Pik=function(z){1-(1-z/sum(z))^(floor(sampleparamtau*length(z)))},#Pik=function(z){z/sum(z)},
+    S=function(z){sample(1:length(z),size=floor(sampleparamtau*length(z)),prob=z/sum(z),replace=TRUE)},
     demarc=function(z){NULL}))}
 #6. Cluster Sampling 
-#ClusterS param=list(proph,probh,tauh)
-ClusterS<-function(param){
+#ClusterS sampleparam=list(proph,probh,tauh)
+ClusterS<-function(sampleparam){
     list(
-      param=param,
+      sampleparam=sampleparam,
       Pik=function(z){
             oo<-order(z);
             N=length(z)
             Nh<-vector()
             nh<-vector()
             Pikk<-rep(NULL,length(z));cum=0
-            for (i in 1:(length(param$proph)-1)){
-              Nh[i]<-floor(param$proph[i]*N);
-              nh[i]<-floor(param$tauh[i]*Nh[i]);
-              Pikk[oo>cum&oo<=cum+Nh[i]]<-param$proph[i]*(nh[i]/Nh[i]);
+            for (i in 1:(length(sampleparam$proph)-1)){
+              Nh[i]<-floor(sampleparam$proph[i]*N);
+              nh[i]<-floor(sampleparam$tauh[i]*Nh[i]);
+              Pikk[oo>cum&oo<=cum+Nh[i]]<-sampleparam$proph[i]*(nh[i]/Nh[i]);
               cum<-cum+Nh[i];}
-            Nh[i+1]<-N-sum(Nh);nh[i+1]<-floor(param$tauh[i+1]*Nh[i+1]);
-            Pikk[oo>cum]<-param$proph[i]*(nh[i+1]/Nh[i+1]);
+            Nh[i+1]<-N-sum(Nh);nh[i+1]<-floor(sampleparam$tauh[i+1]*Nh[i+1]);
+            Pikk[oo>cum]<-sampleparam$proph[i]*(nh[i+1]/Nh[i+1]);
             return(Pikk)},
       S=function(z){
             oo<-order(z);
             N=length(z)
-            nbclus<-length(param$proph)
-            clus<-sample(1:nbclus,size=1,prob=param$probh,replace=FALSE);
-            cum<-floor((sum(param$proph[1:clus])-param$proph[clus])*N)
-            Nh<-floor(param$proph[clus]*N);
-            S=sample((1:N)[oo>cum&oo<=cum+Nh],size=floor(param$tauh[clus]*Nh),replace=FALSE)
+            nbclus<-length(sampleparam$proph)
+            clus<-sample(1:nbclus,size=1,prob=sampleparam$probh,replace=FALSE);
+            cum<-floor((sum(sampleparam$proph[1:clus])-sampleparam$proph[clus])*N)
+            Nh<-floor(sampleparam$proph[clus]*N);
+            S=sample((1:N)[oo>cum&oo<=cum+Nh],size=floor(sampleparam$tauh[clus]*Nh),replace=FALSE)
             return(S)},
         demarc=function(z){
             oo<-order(z);
             N=length(z)
             dem<-vector()
             Nh<-vector();cum<-0;
-            for (i in 1:(length(param$proph)-1)){Nh[i]<-floor(param$proph[i]*N);cum<-cum+Nh[i];dem[i]<-z[oo==cum]}
+            for (i in 1:(length(sampleparam$proph)-1)){Nh[i]<-floor(sampleparam$proph[i]*N);cum<-cum+Nh[i];dem[i]<-z[oo==cum]}
             return(dem)})}
 #7. Systematic sampling with selection proportional to size (and replication allowed when size larger thant step)
-SystematicPPS<-function(param){
-  list(param=param,
+SystematicPPS<-function(sampleparam){
+  list(sampleparam=sampleparam,
     Pik=function(z){
       N<-length(z)
-      n<-floor(N*param$tau)
+      n<-floor(N*sampleparam$tau)
       return(((n*abs(z)/sum(abs(z)))>1)+((n*abs(z)/sum(abs(z)))<=1)*n*abs(z)/sum(abs(z)))},
     S=function(z){
       N<-length(z)
-      n<-floor(N*param$tau)
+      n<-floor(N*sampleparam$tau)
       u<-runif(N)
       oo<-order(u)
       cum<-cumsum(abs(z)[oo])
@@ -154,10 +154,10 @@ SystematicPPS<-function(param){
     demarc=function(z){NULL})}
 
 #1. Take all sampling
-#param : void liste
-takeall<-function(param){
+#sampleparam : void liste
+takeall<-function(sampleparam=NULL){
   list(
-    param=param,
+    sampleparam=sampleparam,
     S=function(z){(1:length(z))[z==1]},
     Pik=function(z){z},
     demarc=function(z){NULL})}  
--- demo/table2.R
+++ demo/table2.R
@@ -3,13 +3,12 @@
 ########################################################
 set.seed(1)#NB: the seed was not set for the table in the publication
 EX=1;SX=1
-Conditionalto<-list(list(sigma=.1,EX=EX,SX=SX),
-            list(sigma=1 ,EX=EX,SX=SX),
-            list(sigma=10,EX=EX,SX=SX));
+Conditionalto<-list(list(N=5000,sigma=.1 ,EX=EX,SX=SX),
+                    list(N=5000,sigma= 1 ,EX=EX,SX=SX),
+                    list(N=5000,sigma= 10,EX=EX,SX=SX));
 #Simulation
 table2_data<-lapply(Conditionalto,function(conditionalto){Simulation_data(popmodelfunction=model.dep.strat2,
                                                                           sampleparam=list(proph=c(.7,.3),tauh=c(1/70,2/15)),
-                                                                          N=5000,
                                                                           theta=c(.5,1,2),
                                                                           xi=2,
                                                                           conditionalto)})
