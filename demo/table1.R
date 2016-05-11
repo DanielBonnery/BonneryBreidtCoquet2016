fa3fbabefe58ff247e0fb2dedc5028f6a393a88a
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-11 12:10:43 -0400

    a

--- demo/table1.R
+++ demo/table1.R
@@ -1,5 +1,6 @@
 set.seed(1)#NB: the seed was not set for the table in the publication
-table1_data<-lapply(c(.1,1,2),function(xi){Simulation_data(popmodelfunction = model.Pareto.bernstrat,
+table1_data<-lapply(c(.1,1,2),function(xi){
+  message(paste0("Run simulations for xi=",xi));Simulation_data(popmodelfunction = model.Pareto.bernstrat,
                                                     theta=4,
                                                     xi=xi,
                                                     conditionalto=list(N=10000,sampleparam=list(tauh=c(0.01,0.1))))})
